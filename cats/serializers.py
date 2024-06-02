3e6e4da8fad8c533bd5090624b060777dc5f9ba2
Ольга Богданова <giredo92@yandex.ru>
2024-06-02 12:00:12 +0300

    3

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+import datetime as dt
 from .models import Achievement, AchievementCat, Cat, Owner
 
 
@@ -13,10 +14,14 @@ class AchievementSerializer(serializers.ModelSerializer):
 class CatSerializer(serializers.ModelSerializer):
     # owner = serializers.StringRelatedField(read_only=True)
     achievements = AchievementSerializer(many=True, required=False)
-
+    age = serializers.SerializerMethodField()
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements',
+                  'age')
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year 
 
     def create(self, validated_data):
         # Если в исходном запросе не было поля achievements
