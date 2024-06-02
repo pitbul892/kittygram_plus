1980beb301e9f5b98b5f550fcebe77cc94aaba7e
Pitbul892 <giredo92@yandex.ru>
2024-06-02 11:34:56 +0300

    2

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,16 +1,50 @@
 from rest_framework import serializers
 
-from .models import Cat, Owner
+from .models import Achievement, AchievementCat, Cat, Owner
+
+
+class AchievementSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Achievement
+        fields = ('id', 'name')
 
 
 class CatSerializer(serializers.ModelSerializer):
+    # owner = serializers.StringRelatedField(read_only=True)
+    achievements = AchievementSerializer(many=True, required=False)
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+
+    def create(self, validated_data):
+        # Если в исходном запросе не было поля achievements
+        if 'achievements' not in self.initial_data:
+            # То создаём запись о котике без его достижений
+            cat = Cat.objects.create(**validated_data)
+            return cat
+        # Уберём список достижений из словаря validated_data и сохраним его
+        achievements = validated_data.pop('achievements')
+
+        # Создадим нового котика пока без достижений, данных нам достаточно
+        cat = Cat.objects.create(**validated_data)
+
+        # Для каждого достижения из списка достижений
+        for achievement in achievements:
+            # Создадим новую запись или получим существующий экземпляр из БД
+            current_achievement, status = Achievement.objects.get_or_create(
+                **achievement
+            )
+            # Поместим ссылку на каждое достижение во вспомогательную таблицу
+            # Не забыв указать к какому котику оно относится
+            AchievementCat.objects.create(achievement=current_achievement, cat=cat)
+        return cat
+
 
 class OwnerSerializer(serializers.ModelSerializer):
+    cats = serializers.StringRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Owner
-        fields = ('first_name', 'last_name', 'cats') 
+        fields = ('first_name', 'last_name', 'cats')
---
+++ requests/requests.py
@@ -0,0 +1,23 @@
+{
+    "first_name": "Roma",
+    "last_name": "Kopytov",
+    "cats": 2
+}
+
+{
+    "name": "Sisya",
+    "color": "black",
+    "birth_year": 2021,
+    "owner": 1
+}
+
+{
+    "name": "Барсик",
+    "color": "White",
+    "birth_year": 2017,
+    "owner": 1,
+    "achievements": [
+        {"name": "поймал мышку"},
+        {"name": "разбил вазу"}
+    ]
+}
